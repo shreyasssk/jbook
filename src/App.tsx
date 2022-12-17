98e17905d26179f23154d4f1cac1106c11601981
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2022-12-17 12:55:35 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
+import { fetchPlugin } from './plugins/fetch-plugin';
+
+export default function App() {
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+
+	useEffect(() => {
+		try {
+			esbuild.build({});
+		} catch (error) {
+			if (error instanceof Error && error.message.includes('initialize')) {
+				esbuild.initialize({
+					worker: true,
+					wasmURL: 'https://unpkg.com/esbuild-wasm@0.16.7/esbuild.wasm',
+				});
+			} else {
+				throw error;
+			}
+		}
+	}, []);
+
+	const onClick = async () => {
+		const result = await esbuild.build({
+			entryPoints: ['index.js'],
+			bundle: true,
+			write: false,
+			plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+			define: {
+				'process.env.NODE_ENV': '"production"',
+				global: 'window',
+			},
+		});
+
+		setCode(result.outputFiles[0].text);
+		eval(result.outputFiles[0].text);
+	};
+
+	return (
+		<div>
+			<textarea
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+			></textarea>
+			<div>
+				<button onClick={onClick}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
 }
-
-export default App;
